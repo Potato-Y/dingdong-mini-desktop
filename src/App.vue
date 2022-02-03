3818ffe08863d4d6c501853b323c70015bcff070
Potato-Y <projectpotatoy@gmail.com>
2022-02-03 21:36:30 +0900

    0.2.5
    첫 로딩시 화면이 정상적으로  표시되지 않는 문제 추가 수정.....

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,15 @@
 
     <!-- width 고정크기 설정 -->
     <div style="width: 1200px">
+      <!-- 콘텐츠 영역 -->
+      <div id="router-content">
+        <router-view />
+        <div id="copy-rights">ⓒ 2022. Potato-Y all rights reserved.</div>
+      </div>
+
       <div style="float: left" ref="hi_korea_Lookup_page_content">
         <HiKoreaLookupPageContent />
       </div>
-      <!-- 콘텐츠 영역 -->
-      <div
-        id="router-content"
-        v-bind:style="{
-          width: 1200 - 4 - hiKoreaLookupPageContentWidth + 'px',
-        }"
-      >
-        <router-view />
-      </div>
-      <div id="copy-rights">ⓒ 2022. Potato-Y all rights reserved.</div>
     </div>
   </div>
 </template>
@@ -26,21 +22,6 @@ import TitleBar from "./components/layout/TitleBar.vue";
 import HiKoreaLookupPageContent from "./components/layout/HiKoreaLookupPageContent.vue";
 export default {
   name: "app",
-  data() {
-    return {
-      hiKoreaLookupPageContentWidth: 0,
-      show: false,
-    };
-  },
-  methods: {
-    GetiKoreaLookupPageContentWidth() {
-      this.hiKoreaLookupPageContentWidth =
-        this.$refs.hi_korea_Lookup_page_content.clientWidth;
-    },
-  },
-  mounted() {
-    this.GetiKoreaLookupPageContentWidth();
-  },
   components: {
     TitleBar,
     HiKoreaLookupPageContent,
